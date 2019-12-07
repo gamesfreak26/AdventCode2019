e57918369b92c7c18621c061b604dd348301d893
Teej <gamesfreak26@gmail.com>
2019-12-07 21:54:32 +1000

    Solve Day 2 Problem 2

--- AdventCalender/Program.cs
+++ AdventCalender/Program.cs
@@ -3,54 +3,51 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using AdventCalender.DayProblems;
+using AdventCalender.DayProblems.Day2;
 
 namespace AdventCalender {
     class Program {
         static void Main(string[] args) {
             string path1 = "InputFiles\\Day1Input.txt";
             string path2 = "InputFiles\\Day2Input.txt";
+            string path2A = "InputFiles\\Day2InputA.txt";
 
             #region Day1Problem1 Region
-            Day1Problem1 day1Problem1 = new Day1Problem1();
-            string[] lines = day1Problem1.ReadLines(path1);
-            var sumofDay1Problem1 = day1Problem1.Sum(lines);
+                Day1Problem1 day1Problem1 = new Day1Problem1();
+
+                string[] lines = day1Problem1.ReadLines(path1);
+
+                var sumofDay1Problem1 = day1Problem1.Sum(lines);
             #endregion ^ Day1Problem1
 
             #region Day1Problem2 Region
-            Day1Problem2 day1Problem2 = new Day1Problem2();
-            var sumOfDay1Problem2 = day1Problem2.Sum(lines);  // Is Wrong
+                Day1Problem2 day1Problem2 = new Day1Problem2();
+                var sumOfDay1Problem2 = day1Problem2.Sum(lines);  // Is Wrong
 
-            var sumOfBothProblems = sumofDay1Problem1 + sumOfDay1Problem2;
+                var sumOfBothProblems = sumOfDay1Problem2;
             #endregion ^ Day1Problem2
 
             #region Day2Problem1 Region
 
-            Day2Problem1 day2Problem1 = new Day2Problem1();
+                Day2Problem1 day2Problem1 = new Day2Problem1();
 
-            //string[] linesForDay2 = day2Problem1.ReadLines(path2);
-            var linesForDay2 = File.ReadAllText(path2);
-            var splitLines = linesForDay2.Split(",");
+                var linesForDay2 = File.ReadAllText(path2);
+                var splitLines = linesForDay2.Split(",");
 
-            List<int> numberList = new List<int>();
+                var intArray = splitLines.Select(x => int.Parse(x)).ToArray();
 
-            //int num = 0;
-            //foreach (var number in splitLines) {
-            //    var lineTrim = number.Trim();
-            //    if (int.TryParse(lineTrim, out num)) {
-            //        numberList.Add(num);
-            //    }
-            //}
+                int[] something = day2Problem1.Something(intArray);
+                #endregion ^ Day2Problem1 Region
 
-            //var intArray = numberList.ToArray();
-            var intArray = splitLines.Select(x => int.Parse(x)).ToArray();
+            #region Day2Problem2 Region
 
-            int[] something = day2Problem1.Something(intArray);
-            Console.WriteLine(something[0]);
+                Day2Problem2 day2Problem2 = new Day2Problem2();
+                var day2Pt2Lines = day2Problem2.ReadLine(path2A);
 
-            //var somethingString = String.Join(",", something);
-            
-            // day2Problem1.Something(numList);
-            #endregion ^ Day2Problem1 Region
+                var splitLinesPt2 = day2Pt2Lines.Split(",");
+                var intArrayPt2 = splitLinesPt2.Select(x => int.Parse(x)).ToArray();
+                var inputs = day2Problem2.FindInputs(intArrayPt2);
+                #endregion ^ Day2Problem2 Region
 
             Console.ReadLine();
         }
