30a57b14377470c044eef4608eee6b874d60e590
Teej <tharanga@maxart.com.au>
2019-12-08 12:19:23 +1000

    Begin Day 5

---
+++ AdventCalender/DayProblems/Day5/Day5Problem1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventCalender.DayProblems.Day5
+{
+    public class Day5Problem1 : IOSystem
+    {
+        ConsoleIOSystem consoleIOSystem = new ConsoleIOSystem();
+
+        public int[] Something(int[] arrayOfInts) {
+            for (int num = 0; num < arrayOfInts.Length; num++) {
+                switch (arrayOfInts[num]) {
+                    case 1: {
+                        var element1 = arrayOfInts[num + 1];
+                        var element2 = arrayOfInts[num + 2];
+                        var element = arrayOfInts[num + 3];
+                        arrayOfInts[element] = arrayOfInts[element1] + arrayOfInts[element2];
+                        num += 3;
+                        break;
+                    }
+                    case 2: {
+                        var element1 = arrayOfInts[num + 1];
+                        var element2 = arrayOfInts[num + 2];
+                        var multiplicationElement = arrayOfInts[num + 3];
+                        arrayOfInts[multiplicationElement] = arrayOfInts[element1] * arrayOfInts[element2];
+                        num += 3;
+                        break;
+                    }
+                    case 3: {
+                        var userInput = Console.ReadLine();
+                        var element1 = arrayOfInts[num + 1];
+                        if (int.TryParse(userInput, out num)) {
+                            arrayOfInts[element1] = num;
+                        }
+                        break;
+                    }
+                    case 4: {
+
+                        break;
+                    }
+                    case 99:
+                        return arrayOfInts;
+                }
+            }
+            return arrayOfInts;
+        }
+
+        public int GetInput() {
+            var input = consoleIOSystem.GetInput();
+            return input;
+        }
+
+        public void WriteOutput(int parameter) {
+            consoleIOSystem.WriteOutput(parameter);
+        }
+    }
+}
---
+++ AdventCalenderTests/Day5Problem1Tests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventCalenderTests
+{
+    public class Day5Problem1Tests
+    {
+    }
+}
